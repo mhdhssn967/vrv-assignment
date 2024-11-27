6026b0bc42ca5923b5ad96a970f28d8ac96294c4
mhdhssn967 <mhdhssn967@gmail.com>
2024-11-27 09:24:19 +0530

    width

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -16,7 +16,7 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 440,
+    width: '80vw',
     bgcolor: 'background.paper',
     border: '2px solid #000',
 };
